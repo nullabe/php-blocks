83f63d32d033b6faf05fb7f5580096eddd4270f9
Antoine Belluard <abelluard@outlook.com>
2018-04-04 21:52:51 +0200

    valid project composer.json

---
+++ src/Domain/Model/Blockchain.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace Nullabe\PhpBlocks\Domain\Model;
+
+
+class Blockchain
+{
+
+}
---
+++ src/Test/Domain/Model/BlockchainTest.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types=1);
+
+namespace Nullabe\PhpBlocks\Domain\Model;
+
+class BlockchainTest
+{
+
+}
