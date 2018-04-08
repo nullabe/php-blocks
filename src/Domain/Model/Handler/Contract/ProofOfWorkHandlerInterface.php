b7692005bce64e10871720cce0f4622f27d5c27d
Antoine Belluard <abelluard@outlook.com>
2018-04-08 12:38:11 +0200

    Added methods to ProofOfWorkHandlerInterface

--- src/Domain/Model/Handler/Contract/ProofOfWorkHandlerInterface.php
+++ src/Domain/Model/Handler/Contract/ProofOfWorkHandlerInterface.php
@@ -5,5 +5,11 @@ namespace Nbe\PhpBlocks\Domain\Model\Handler\Contract;
 
 interface ProofOfWorkHandlerInterface
 {
+    public function proofOfWork(int $lastProof): int;
+
+    public function hashByTwoIntMultiplication(int $x, int $y): string;
+
+    public static function validateProof(string $hash): bool;
 
 }
+
