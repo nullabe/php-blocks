47859a846e00410718312ff37a17826e70f77c0f
Antoine Belluard <abelluard@outlook.com>
2018-06-28 22:42:30 +0200

    [Infra] Added Symfony 4, root route functional

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Kernel;
+use Nbe\PhpBlocks\Infrastructure\Kernel;
 use Symfony\Component\Debug\Debug;
 use Symfony\Component\Dotenv\Dotenv;
 use Symfony\Component\HttpFoundation\Request;
@@ -20,20 +20,20 @@ $debug = (bool) ($_SERVER['APP_DEBUG'] ?? ('prod' !== $env));
 
 if ($debug) {
     umask(0000);
+
     Debug::enable();
 }
+
 if ($trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? false) {
     Request::setTrustedProxies(explode(',', $trustedProxies), Request::HEADER_X_FORWARDED_ALL ^ Request::HEADER_X_FORWARDED_HOST);
 }
+
 if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? false) {
     Request::setTrustedHosts(explode(',', $trustedHosts));
 }
 
 $kernel = new Kernel($env, $debug);
-
 $request = Request::createFromGlobals();
-
 $response = $kernel->handle($request);
 $response->send();
-
 $kernel->terminate($request, $response);
