d7b4047d1bc7401a6e2bce78983f5381e2c9e852
Antoine Belluard <abelluard@outlook.com>
2018-11-10 19:52:44 +0100

    refactor(infra): Renamed Blockchain json formatter and use of interface

--- src/Infrastructure/Storage/File/Formatter/BlockchainJsonFormatter.php
+++ src/Infrastructure/Storage/File/Formatter/BlockchainJsonFormatter.php
@@ -1,17 +1,18 @@
 <?php
 declare(strict_types=1);
 
-namespace Nbe\PhpBlocks\Infrastructure\Storage\File\Format;
+namespace Nbe\PhpBlocks\Infrastructure\Storage\File\Formatter;
 
 use Nbe\PhpBlocks\Domain\Model\Entity\Blockchain;
+use Nbe\PhpBlocks\Domain\Model\Formatter\Contract\BlockchainFormatter;
 use Nbe\PhpBlocks\Domain\Model\Normalizer\BlockchainNormalizer;
 
 /**
- * Class BlockchainJson
+ * Class BlockchainJsonFormatter
  *
- * @package Nbe\PhpBlocks\Infrastructure\Storage\File\Format
+ * @package Nbe\PhpBlocks\Infrastructure\Storage\File\Formatter
  */
-class BlockchainJson
+class BlockchainJsonFormatter implements BlockchainFormatter
 {
 
     /**
@@ -19,7 +20,7 @@ class BlockchainJson
      *
      * @return string
      */
-    public function toJson(Blockchain $blockchain): string
+    public function format(Blockchain $blockchain): string
     {
         return \json_encode(BlockchainNormalizer::normalize($blockchain));
     }
