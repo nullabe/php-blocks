6c4ee6a89253599a4d932f3c2aafb9885267886c
Antoine Belluard <abelluard@outlook.com>
2018-11-10 15:07:35 +0100

    test(domain): Build blockchain instance from state

--- tests/Domain/Model/Normalizer/BlockchainNormalizerTest.php
+++ tests/Domain/Model/Normalizer/BlockchainNormalizerTest.php
@@ -6,6 +6,8 @@ namespace Nbe\PhpBlocks\Tests\Domain\Model\Normalizer;
 use Nbe\PhpBlocks\Domain\Model\Entity\Blockchain;
 use Nbe\PhpBlocks\Domain\Model\Normalizer\BlockchainNormalizer;
 use Nbe\PhpBlocks\Domain\Model\Normalizer\BlockNormalizer;
+use Nbe\PhpBlocks\Domain\Model\Normalizer\TransactionNormalizer;
+use Nbe\PhpBlocks\Domain\Model\State\BlockchainState;
 use PHPUnit\Framework\TestCase;
 
 /**
@@ -13,22 +15,40 @@ use PHPUnit\Framework\TestCase;
  */
 final class BlockchainNormalizerTest extends TestCase
 {
-    public function testBlockchainCanBeNormalize()
+    /**
+     * @throws \Nbe\PhpBlocks\Domain\Exception\BlockDenormalizeException
+     * @throws \Nbe\PhpBlocks\Domain\Exception\BuildBlockchainStateException
+     * @throws \Nbe\PhpBlocks\Domain\Exception\TransactionDenormalizeException
+     */
+    public function testBlockchainCanBeNormalizeAndRebuiltFromState()
     {
         $blockchain = Blockchain::getInstance();
 
         foreach ($blockchain->getChain() as $index => $block) {
             $blocks[] = BlockNormalizer::normalize($block);
         }
+        foreach ($blockchain->getTransactionStack() as $transaction) {
+            $transactions[] = TransactionNormalizer::normalize($transaction);
+        }
 
         $this->assertEquals(
             [
                 'uuid' => $blockchain->uuid(),
                 'chain' => $blocks ?? [],
+                'transactionStack' => $transactions ?? [],
                 'lastBlock' => BlockNormalizer::normalize($blockchain->getLastBlock()),
                 'length' => $blockchain->getNextIndex() - 1,
             ],
-            BlockchainNormalizer::normalize($blockchain)
+            $blockchainData = BlockchainNormalizer::normalize($blockchain)
+        );
+
+        // Only test uuid to be sure Blockchain instance has been updated.
+        $blockchainData['uuid'] = 'test';
+        $blockchainState = new BlockchainState($blockchainData);
+
+        $this->assertEquals(
+            'test',
+            Blockchain::getInstance($blockchainState)->uuid()
         );
     }
 }
